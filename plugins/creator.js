9a0b7b93ab4e4a1aba926465b8f45d2540134017
feedzBOT <95519752+feedzBOT@users.noreply.github.com>
2021-12-12 20:05:36 +0800

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,7 +1,7 @@
 function handler(m) {
   // Ini cuma contoh, jgn di uncomment -_-
   // F this.sendContact(m.chat, '62815158600891', 'Nurutomo', m)
-  this.sendContact(m.chat, '62887436568613', 'ブラック', m)
+  this.sendContact(m.chat, '6288705834498', 'My owner', m)
 }
 //handler.help = ['owner', 'creator']
 //handler.tags = ['info']
