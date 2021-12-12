83bbd1f747bd622ebbfccd3c98fc2cd05922c816
feedzBOT <95519752+feedzBOT@users.noreply.github.com>
2021-12-12 20:21:14 +0800

    Update tagbot.js

--- plugins/tagbot.js
+++ plugins/tagbot.js
@@ -5,7 +5,7 @@ let me = conn.user.name
 let teks = `
 ${pickRandom([` _Saya disini Master_`, `_Saya hadir_`, ` _Apakah Master mencari saya??_`, ` _Yapp Master_`, `*_© H i - F E 3 D Z_*\n\nAda hanya untuk melayani anda`])}
 `.trim()
-conn.fakeReply(m.chat, teks, '0@s.whatsapp.net', `${me}  Love  U n x`, 'status@broadcast')
+conn.fakeReply(m.chat, teks, '0@s.whatsapp.net', `${me} ¯\_(ツ)_/¯`, 'status@broadcast')
 }
 handler.customPrefix = /6288705834498|hifeedz/
 handler.command = new RegExp
