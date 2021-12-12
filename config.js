6a2f8e21a4a374e08e1e45f5e878106bd3b9ad55
feedzBOT <95519752+feedzBOT@users.noreply.github.com>
2021-12-12 21:12:49 +0800

    Update config.js

--- config.js
+++ config.js
@@ -27,7 +27,7 @@ const spack = fs.readFileSync("lib/exif.json")
 const stickerpack = JSON.parse(spack)
 if (stickerpack.spackname == '') {
   var sticker_name = 'Botti BOT'
-  var sticker_author = '© H i - F E 3 D Z'
+  var sticker_author = 'H i F E 3 D Z'
 } else {
   var sticker_name = stickerpack.spackname
   var sticker_author = stickerpack.sauthor
