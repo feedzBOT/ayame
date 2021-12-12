db5a7da49934614521e8a2544cf5522f34e71d62
feedzBOT <95519752+feedzBOT@users.noreply.github.com>
2021-12-12 20:19:54 +0800

    Update tagbot.js

--- plugins/tagbot.js
+++ plugins/tagbot.js
@@ -3,11 +3,11 @@ if (!isOwner) throw false
 let name = conn.getName(m.sender)
 let me = conn.user.name
 let teks = `
-${pickRandom([` _Saya disini Master_`, ` _Ayamee hadir_`, ` _Apakah Master mencari saya??_`, ` _Yapp Master_`, `*_百鬼あやめ_*\n\nAda hanya untuk melayani Master  U n x`])}
+${pickRandom([` _Saya disini Master_`, `_Saya hadir_`, ` _Apakah Master mencari saya??_`, ` _Yapp Master_`, `*_© H i - F E 3 D Z_*\n\nAda hanya untuk melayani anda`])}
 `.trim()
 conn.fakeReply(m.chat, teks, '0@s.whatsapp.net', `${me}  Love  U n x`, 'status@broadcast')
 }
-handler.customPrefix = /6289524339740|ayame/
+handler.customPrefix = /6288705834498|hifeedz/
 handler.command = new RegExp
 
 module.exports = handler
